793a998050f8e783ef4bad9a6a5a53f1bf65a367
samiksha Shende <shende.samiksha@rediffmail.com>
2020-10-05 15:43:57 +0530

    Added : Count the number of people having same birth month

--- dictionary.sh
+++ dictionary.sh
@@ -4,6 +4,8 @@ echo "Welcome to dictionary problem"
 
 #!/bin/bash/
 
+#Problem1 : Roll the dice till one of the number has reach 10 
+
 declare -A num
 
 one=0
@@ -81,3 +83,138 @@ done
 echo "Number = $maxInd reached maximum time ( $max )"
 
 echo "Number = $minInd reached minimum  time ( $min )"
+
+
+#Problem2 : Count number of people having same birth month between year 92-93
+
+
+Month() {
+
+month=$(( $RANDOM % 12 ))
+
+        case $month in
+
+        0)
+        jan=$(( $jan + 1 ))
+        mon[1]=$jan
+        ;;
+
+        1)
+        feb=$(( $feb + 1 ))
+        mon[2]=$feb
+        ;;
+
+        2)
+        mar=$(( $mar + 1 ))
+        mon[3]=$mar
+        ;;
+
+        3)
+        apr=$(( $apr + 1 ))
+        mon[4]=$apr
+        ;;
+
+        4)
+        may=$(( $may + 1 ))
+        mon[5]=$may
+        ;;
+
+        5)
+        jun=$(( $jun + 1 ))
+        mon[6]=$jun
+        ;;
+
+        6)
+        jul=$(( $jul + 1 ))
+        mon[7]=$jul
+        ;;
+
+	7)
+        aug=$(( $aug + 1 ))
+        mon[8]=$aug
+        ;;
+
+        8)
+        sep=$(( $sep + 1 ))
+        mon[9]=$sep
+        ;;
+
+        9)
+        oct=$(( $oct + 1 ))
+        mon[10]=$oct
+        ;;
+
+        10)
+        nov=$(( $nov + 1 ))
+        mon[11]=$nov
+        ;;
+
+        11)
+        dec=$(( $dec + 1 ))
+        mon[12]=$dec
+        ;;
+
+        esac
+}
+
+declare -A mon
+
+year92=0
+year93=1
+
+jan=0
+feb=0
+mar=0
+apr=0
+may=0
+jun=0
+jul=0
+aug=0
+sep=0
+oct=0
+nov=0
+dec=0
+
+mon[1]=0
+mon[2]=0
+mon[3]=0
+mon[4]=0
+mon[5]=0
+mon[6]=0
+mon[7]=0
+mon[8]=0
+mon[9]=0
+mon[10]=0
+mon[11]=0
+mon[12]=0
+
+n=0
+
+while [ $n -lt 50 ]
+do
+
+random=$(( $RANDOM % 2 ))
+
+        case $random in
+
+        $year92)
+        Month
+        ;;
+
+        $year93)
+        Month
+        ;;
+
+        esac
+
+        n=$(( $n + 1 ))
+
+done
+
+for i in `seq ${#mon[@]}`
+do
+
+echo "$i month : ${mon[$i]} people"
+
+done
+
